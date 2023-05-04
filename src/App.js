2fffe3b7185e73ece90d950a530cf58b02ab0b37
Muhammad Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-05-04 12:08:04 +0800

    feat: add catalog & search feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div>
+          <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} alt="" className="Movie-image" />
+            <div className="Movie-date"> release: {movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+          </div>
+        </div>
+      );
+    })
+  }
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AzMOVIE</h1>
+        <h1>AzetMovie</h1>
+        <input
+          type="text"
+          className="Movie-search"
+          placeholder="Find your movie ..."
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
